b75daefc5c2492becf8ccc0aa25bccc1300c943e
amandagab <amandagabsa@gmail.com>
2022-06-13 14:02:14 -0300

    versao 2.0

--- src/server.js
+++ src/server.js
@@ -3,15 +3,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist','Projeto-Consultorio-Front-2.0'));
-res.sendFile(path.join(__dirname,'dist','Projeto-Consultorio-Front-2.0','index.html'));
-// // Serve only the static files form the dist directory
-// app.use(express.static('./dist/Projeto-Consultorio-Front-2.0'));
 //
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/Projeto-Consultorio-Front-2.0/'}),
-// );
+// app.use(express.static(path.join(__dirname, 'DEPS','Projeto-Consultorio-Front-2.0'));
+// res.sendFile(path.join(__dirname,'DEPS','Projeto-Consultorio-Front-2.0','index.html'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/hml-consultorio-app'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/hml-consultorio-app/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
