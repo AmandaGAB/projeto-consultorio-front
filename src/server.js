7ba2a653bd4c7dc855413924fec8921253e5ea23
amandagab <amandagabsa@gmail.com>
2022-06-13 13:27:03 -0300

    versao 2.0

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('./dist/'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/Projeto-Consultorio-Front-2.0'));
 
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: 'dist/' });
+app.get('*', (req, res) => {
+    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
-
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
