fd4a56d9a250ab677b36fb9ea58f01b4c78baf99
amandagab <amandagabsa@gmail.com>
2022-06-13 13:19:52 -0300

    versao 2.0

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/Projeto-Consultorio-Front-2.0'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Projeto-Consultorio-Front-2.0'}),
+    res.sendFile('index.html', {root: 'dist/Projeto-Consultorio-Front-2.0/'}),
 );
 
 // Start the app by listening on the default Heroku port
